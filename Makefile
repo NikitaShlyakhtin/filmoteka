35889b8e65b5f1c20505f7d8689b7b8981838888
Nikita Shlyakhtin <nikitashlyakhtin@mail.ru>
2024-03-13 12:05:54 +0300

    HTTP server setup

---
+++ Makefile
@@ -0,0 +1,21 @@
+# Include variables from the .envrc file
+include .envrc
+
+# ==================================================================================== #
+# HELPERS
+# ==================================================================================== #
+
+## help: print this help message
+.PHONY: help
+help:
+	@echo 'Usage:'
+	@sed -n 's/^##//p' ${MAKEFILE_LIST} | column -t -s ':' | sed -e 's/^/ /'
+
+# ==================================================================================== #
+# DEVELOPMENT
+# ==================================================================================== #
+
+## run/api: run the cmd/api application
+.PHONY: run/api
+run/api:
+	go run ./cmd/api
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"os"
+	"sync"
+
+	"filmoteka/internal/jsonlog"
+)
+
+type config struct {
+	port    int
+	env     string
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
+}
+
+type application struct {
+	config config
+	logger *jsonlog.Logger
+	wg     sync.WaitGroup
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
+	flag.Parse()
+
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	err := app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
+	}
 }
